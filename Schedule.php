6a26691ff65e6a0969a2289020b8eb3b2a47d5a7
Ben Flynn <bennaflynn@gmail.com>
2018-03-26 16:16:13 -0700

    Created the schedule display using PHP

--- Schedule.php
+++ Schedule.php
@@ -13,26 +13,117 @@
 <?php 
 $scheduleFile = "./data/ssd-schedule-2017-2018.csv";
 
+$sep = [];
+$oct = [];
+$nov = [];
+$dec = [];
+$jan = [];
+$feb = [];
+$mar = [];
+$apr = [];
+
+
 //The r says read I think
 if(($handle = fopen($scheduleFile,"r")) !== FALSE) {
     while(($data = fgetcsv($handle,1000,",")) !== FALSE) {
         $number = count($data);
-        echo "<div>";
+
         for ($cell = 0; $cell < $number; $cell++) {
-            if($data[0] == "Monday" ||
-                $data[0] == "Tuesday" ||
-                $data[0] == "Wednesday" ||
-                $data[0] == "Thursday" ||
-                $data[0] == "Friday" ||
-                $data[0] == "Saturday" ||
-                $data[0] == "Sunday" ) {
-                    echo $data[$cell] . "<br>\n";
-                }
+
+            switch(substr($data[1],0,3)) {
+                
+                case "Sep": 
+                    array_push($sep,$data[$cell]);
+                    break;
+                case "Oct": 
+                    array_push($oct,$data[$cell]);
+                    break;
+                case "Nov": 
+                    array_push($nov,$data[$cell]);
+                    break;
+                case "Dec": 
+                    array_push($dec,$data[$cell]);
+                    break;
+                case "Jan": 
+                    array_push($jan,$data[$cell]);
+                    break;
+                case "Feb": 
+                    array_push($feb,$data[$cell]);
+                    break;
+                case "Mar": 
+                    array_push($mar,$data[$cell]);
+                    break;
+                case "Apr": 
+                    array_push($apr,$data[$cell]);
+                    break;
+            }
             
         }
-        echo "</div>";
+       
     }
+  
     fclose($handle);
+    
+}
+
+function DisplayMonth($month, $monthName) {
+    $dataCount = 0;
+    $weekDays = Array("Monday", "Tuesday", "Wednesday","Thursday","Friday");
+    echo "<h1 class='monthHead'>" . $monthName . "</h1>";
+    echo "<div class='month'>";
+    for($d=0; $d < count($weekDays); $d++) {
+        echo "<div class='dayHead'> <h3>" . $weekDays[$d] . "</h3></div>";
+    }
+
+    $dayCount = 0;
+
+    for($s=0; $s < count($month); $s++) {
+        //data comes in packets of 5 (one is blank)
+        if($s == 0) {
+            if($month[$s] == "Tuesday") {
+                echo "<div class='day'></div>";
+            } else if($month[$s] == "Wednesday") {
+                echo "<div class='day'></div>";
+                echo "<div class='day'></div>";
+            } else if($month[$s] == "Thursday") {
+                echo "<div class='day'></div>";
+                echo "<div class='day'></div>";
+                echo "<div class='day'></div>";
+            } else if ($month[$s] == "Friday") {
+                echo "<div class='day'></div>";
+                echo "<div class='day'></div>";
+                echo "<div class='day'></div>";
+                echo "<div class='day'></div>";
+            }
+
+        }
+        if($dataCount == 0) {
+            echo "<div class='day'>";
+            echo "<ul>";
+        }
+        if($month[$s] !== "") {
+            echo "<li>" . $month[$s] . "</li>";  
+        }
+        
+        
+        $dataCount++;
+        if($dataCount == 5) {
+            echo "</div>";
+            echo "</ul>";
+            $dataCount = 0;
+        }
+
+    }
+    echo "</div>";
+
+    echo "<hr>";
+}
+
+$months = Array($sep,$oct,$nov,$dec,$jan,$feb,$mar,$apr);
+$monthNames = Array("September","October","November","December","January","February","March","April");
+
+for($m=0; $m < count($months); $m++) {
+    DisplayMonth($months[$m],$monthNames[$m]);
 }
 
 ?>
